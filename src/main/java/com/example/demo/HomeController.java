22129274d1fda16c6913cfd45c285f3f98fdad53
greendb16 <greendb16@gmail.com>
2019-07-16 11:22:44 -0400

    First and Final

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    ActorRepository actorRepository;
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @RequestMapping("/")
+    public String index(Model model){
+        //Create new actor
+        Actor actor = new Actor();
+        actor.setName("Sandra Bullock");
+        actor.setRealname("Sandra Mae Bullowski");
+
+        //Create movie
+        Movie movie = new Movie();
+        movie.setTitle("Emoji Movie");
+        movie.setYear(2017);
+        movie.setDescription("About Emojis...");
+
+        //Add movies to list
+        Set<Movie> movies = new HashSet<Movie>();
+        movies.add(movie);
+
+        actor.setMovies(movies);
+
+        //Save actor to database
+        actorRepository.save(actor);
+
+        //Send actors to template
+        model.addAttribute("actors", actorRepository.findAll());
+        return "index";
+    }
+}
